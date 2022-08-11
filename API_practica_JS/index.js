0b65b1f9fe8d8d770ee8ac90fd22c1833d6ca8c8
tomasnapolitano <t97napolitano@gmail.com>
2022-08-11 15:58:47 -0300

    Ejercicio extra de práctica con API y Boostrap

---
+++ API_practica_JS/index.js
@@ -0,0 +1,26 @@
+const url = "https://jsonplaceholder.typicode.com/users";
+
+const listUsers = async () =>{
+    const response= await fetch(url);
+    const users = await response.json();
+
+    let tableBody =``;
+    users.forEach((user,index) => {
+        // console.log(user);
+        tableBody+=`<tr>
+        <td>${user.id}</td>
+        <td>${user.name}</td>
+        <td>${user.username}</td>
+        <td>${user.email}</td>
+        <td>${user.website}</td>
+        </tr>
+        `
+    });
+       // document.getElementById("tableBody_users").innerHTML = tableBody;
+       tableBody_users.innerHTML = tableBody; 
+};
+
+
+window.addEventListener("load",function(){
+    listUsers();
+});
